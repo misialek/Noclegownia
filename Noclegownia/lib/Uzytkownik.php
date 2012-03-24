eca2cb3fd4fee1f65311771f905e518dcacaa4d5
uriziel <uriziel@ks3094672.kimsufi.com>
2012-03-24 15:13:42 +0100

    Dodane zarzadzanie noclegownia, naprawiony blad w pliku db.php sprawiajacy, ze kodowanie polaczenia z baza jest ustawione na latin1

---
+++ Noclegownia/lib/Uzytkownik.php
@@ -0,0 +1,77 @@
+<?php
+include_once "../db.php";
+
+class Uzytkownik{
+  private $id;
+  private $typ_konta;
+    const UZYTKOWNIK = 0;
+    const RECEPCJONISTA = 1;
+    const ADMIN = 2;
+  private $imie;
+  private $nazwisko;
+  private $login;
+  private $haslo;
+  private $email;
+  private $kod;
+  private $data;
+  private $status;
+  private $id_miejsce;
+  const WSZYSTKIE_KOLUMNY = "uzytkownik.id, uzytkownik.typ_konta, uzytkownik.imie, uzytkownik.nazwisko, uzytkownik.login, uzytkownik.haslo, uzytkownik.email, uzytkownik.kod, uzytkownik.data, uzytkownik.status, uzytkownik.id_miejsce";
+
+  function __construct(){
+    $args = func_get_args();
+    if(count($args) == 1 && is_array($args)){
+      $this->id = $args[0]['id'];
+      $this->typ_konta = $args[0]['typ_konta'];
+      $this->imie = $args[0]['imie'];
+      $this->nazwisko = $args[0]['nazwisko'];
+      $this->login = $args[0]['login'];
+      $this->haslo = $args[0]['haslo'];
+      $this->email = $args[0]['email'];
+      $this->kod = $args[0]['kod'];
+      $this->data  = $args[0]['data'];
+      $this->status = $args[0]['status'];
+      $this->id_miejsce = $args[0]['id_miejsce'];
+    } else{
+      $this->id = $args[0];
+      $this->typ_konta = $args[1];
+      $this->imie = $args[2];
+      $this->nazwisko = $args[3];
+      $this->login = $args[4];
+      $this->haslo = $args[5];
+      $this->email = $args[6];
+      $this->kod = $args[7];
+      $this->data  = $args[8];
+      $this->status = $args[9];
+      $this->id_miejsce = $args[10];
+    }
+  }
+
+  public function __set($key,$val) {
+    $this->$key=$val;
+  }
+  public function __get($key) {
+    $vars = get_object_vars($this);
+    foreach($vars as $varKey => $var){
+      if($key == $varKey) return $var;
+    }
+    throw new Exception("No ". $key ." property in object");
+  }
+
+  public static function doSelectOneByLogin($login){
+    global $polaczenie;
+    $query = sprintf("SELECT " . self::WSZYSTKIE_KOLUMNY . " FROM uzytkownik
+                      WHERE login = '%s';",
+                      mysql_real_escape_string($login));
+    $result = mysql_query($query,$polaczenie);
+    if(!$result OR mysql_num_rows($result) != 1 ){
+      header('Location: ../index.php ');
+      die;
+      //throw new Exception("Blad pobierania uzytkownika");
+    }
+    $row = mysql_fetch_assoc($result);
+    return $uzytkownik = new Uzytkownik($row);
+  }
+
+}
+?>
---
+++ Noclegownia/zarzadzanie_noclegownia/index.php
@@ -0,0 +1,234 @@
+<?
+include_once '../lib/Uzytkownik.php';
+include_once '../lib/Noclegownia.php';
+include_once '../lib/Pokoj.php';
+include_once '../lib/Rezerwacja.php';
+session_start();
+$admin = new AdministrationActionClass();
+$admin->init($_SESSION['login'], isset($_GET['akcja'])?$_GET['akcja']:"");
+
+class AdministrationActionClass{
+  private $user;
+  public function init($login, $akcja){
+    try{
+      $this->user = Uzytkownik::doSelectOneByLogin($login);
+      $typ_konta = $this->user->__get('typ_konta');
+      if(!$typ_konta == Uzytkownik::ADMIN || !$typ_konta == Uzytkownik::RECEPCJONISTA){
+        header('Location: uzytkownik.php ');
+      }
+      switch($akcja){
+        case "dodajPokoj":
+          $this->dodajPokoj();
+          break;
+        case "listujPokoje":
+          $this->listujPokoje();
+          break;
+        case "listujRezerwacje":
+          $this->listujRezerwacje();
+          break;
+        default:
+          include "widok/indeks.php";
+          die;
+      }
+    } catch(Exception $e){
+      include "../error500.php";
+    }
+  }
+
+  public function dodajPokoj(){
+    $dostepneNoclegownie = Noclegownia::doSelectDostepneNoclegownie($this->user->__get('id'), $this->user->__get('typ_konta'));
+    if(isset($_POST['nowy_pokoj'])){
+      $this->stworzNowyPokoj($dostepneNoclegownie);
+      include 'widok/zmianaWykonanaPomyslnie.php';
+      die;
+    }else{
+      include 'widok/nowy_pokoj.php';
+      die;
+    }
+  }
+
+  public function listujRezerwacje(){
+    $dostepneNoclegownie = Noclegownia::doSelectDostepneNoclegownie($this->user->__get('id'), $this->user->__get('typ_konta'));
+    if(isset($_POST['lista_rezerwacji'])){
+      $wszystkieAktywneRezerwacje = Rezerwacja::pobierzRezerwacjeDlaZarzadncy($this->user);
+      foreach($wszystkieAktywneRezerwacje as $rezerwacja){
+        $rezerwacja = $rezerwacja['rezerwacja'];
+        if(isset($_POST["akceptuj_".$rezerwacja->__get('id_rez')])){
+          $rezerwacja->__set('status', Rezerwacja::ZAAKCEPTOWANA);
+          $rezerwacja->zapiszZmianeStatusu();
+        }
+        if(isset($_POST["odrzuc_".$rezerwacja->__get('id_rez')])){
+          $rezerwacja->__set('status', Rezerwacja::ODRZUCONA);
+          $rezerwacja->zapiszZmianeStatusu();
+        }
+        include 'widok/zmianaWykonanaPomyslnie.php';
+        die;
+      }
+    }
+    $wszystkieAktywneRezerwacje = Rezerwacja::pobierzRezerwacjeDlaZarzadncy($this->user);
+    $aktywneRezerwacje = $this->podzielRezerwacjeNaNoclegownie($wszystkieAktywneRezerwacje);
+    include 'widok/listuj_rezerwacje.php';
+  }
+
+  public function pokazPokoj(){
+    $dostepneNoclegownie = Noclegownia::doSelectDostepneNoclegownie($this->user->__get('id'), $this->user->__get('typ_konta'));
+    $wszystkieDostepnePokoje = $this->pobierzDostepnePokoje($dostepneNoclegownie);
+    foreach($wszystkieDostepnePokoje as $pokoj){
+      $nocleg = null;
+      foreach($dostepneNoclegownie as $noclegownia){
+        if($pokoj->__get('id_miejsce') == $noclegownia->__get('id')) $nocleg = $noclegownia;
+      }
+      if($pokoj->__get('id_pok') == $_GET['pokoj']){
+        $_SESSION['modyfikowanePokoje'] =  array($pokoj->__get('id_pok'));
+        include "widok/Podglad_pokoju.php";
+        die;
+      }
+    }
+    include "widok/pokoj_nie_istnieje.php";
+    die;
+  }
+
+  public function listujPokoje(){
+    if(isset($_GET['pokoj']) && !isset($_POST['lista_pokoi'])){
+      $this->pokazPokoj();
+      die;
+    }
+    $dostepneNoclegownie = Noclegownia::doSelectDostepneNoclegownie($this->user->__get('id'), $this->user->__get('typ_konta'));
+    if(isset($_POST['lista_pokoi'])){
+      $wszystkieDostepnePokoje = $this->pobierzDostepnePokoje($dostepneNoclegownie);
+      $this->wykonajZmianyNaPokojach($wszystkieDostepnePokoje);
+      include 'widok/zmianaWykonanaPomyslnie.php';
+      die;
+    }
+    $wszystkieDostepnePokoje = $this->pobierzDostepnePokoje($dostepneNoclegownie);
+    $dostepnePokoje = array();
+    $dostepnePokojeIds = array();
+    foreach($wszystkieDostepnePokoje as $pokoj){
+      $dostepnePokoje[$pokoj->__get('id_miejsce')][] = $pokoj;
+      $dostepnePokojeIds[] = $pokoj->__get('id_pok');
+    }
+    $_SESSION['modyfikowanePokoje'] = $dostepnePokojeIds;
+    include 'widok/listuj_pokoje.php';
+    die;
+  }
+
+  private function stworzNowyPokoj($dostepneNoclegownie){
+      $isFileUploaded = isset($_FILES['zdjecie']['tmp_name']) && $_FILES['zdjecie']['tmp_name'] != "";
+      $isFileUploaded = $isFileUploaded?in_array($_FILES['zdjecie']['type'], array('image/jpeg', 'image/png', 'image/gif')):false;
+      $zdjecie = $isFileUploaded?file_get_contents($_FILES['zdjecie']['tmp_name']):"";
+      $tv = isset($_POST['tv'])?1:0;
+      $lodowka = isset($_POST['lodowka'])?1:0;
+      $wc = isset($_POST['wc'])?1:0;
+      $prszynic = isset($_POST['prszynic'])?1:0;
+      $wanna = isset($_POST['wanna'])?1:0;
+      $jacuzzi = isset($_POST['jacuzzi'])?1:0;
+      $klimatyzacja = isset($_POST['klimatyzacja'])?1:0;
+      $internet = isset($_POST['internet'])?1:0;
+      $idNoclegownia = isset($_POST['noclegownia'])?$_POST['noclegownia']:NULL;
+
+      $this->sprawdzDostepUseraDoNoclegowni($dostepneNoclegownie, $idNoclegownia);
+      $nowyPokoj = new Pokoj($zdjecie, '', $idNoclegownia, $tv, $lodowka, $wc, $prszynic, $wanna, $jacuzzi, $klimatyzacja, $internet, '');
+      $nowyPokoj->zapiszNowyPokoj();
+  }
+
+  private function sprawdzDostepUseraDoNoclegowni($dostepneNoclegownie, $idNoclegownia){
+      $userMaDostep = false;
+      if(isset($_POST['noclegownia'])){
+        foreach($dostepneNoclegownie as $noclegownia){
+          if($noclegownia->__get('id') == $idNoclegownia){
+            $userMaDostep=true;
+            break;
+          }
+        }
+      }
+      if(!$userMaDostep){
+        throw new Exception("Uzytkownik nie ma dostepu do tej noclegowni");
+      }
+      return true;
+  }
+
+  private function wykonajZmianyNaPokojach($wszystkieDostepnePokoje){
+      $dostepnePokojeIds = isset($_SESSION['modyfikowanePokoje'])?$_SESSION['modyfikowanePokoje']:array();
+      unset($_SESSION['modyfikowanePokoje']);
+      foreach($wszystkieDostepnePokoje as $key => $pokoj){
+        if(!in_array($pokoj->__get('id_pok'),$dostepnePokojeIds)){ //Gdyby przypadkiem pojawił się w międzyczasie jakiś pokój
+          array_slice($wszystkieDostepnePokoje, $key, 1);      //mało prawdopodobne, ale zawsze lepiej się upewnić
+          continue;
+        }
+        if(isset($_POST["delete_".$pokoj->__get('id_pok')])){
+          $pokoj->usun();
+          continue;
+        }
+        $pokoj = $this->zmienUstawieniaPokojuWZaleznosciOdZapostowanychDanych($pokoj);
+        $pokoj->zapiszZmianyWPokoju();
+      }
+  }
+
+  private function zmienUstawieniaPokojuWZaleznosciOdZapostowanychDanych($pokoj){
+      if(isset($_POST["tv_".$pokoj->__get('id_pok')])){
+        $pokoj->__set('tv', 1);
+      } else {
+        $pokoj->__set('tv', 0);
+      }
+      if(isset($_POST["lodowka_".$pokoj->__get('id_pok')])){
+        $pokoj->__set('lodowka', 1);
+      } else {
+        $pokoj->__set('lodowka', 0);
+      }
+      if(isset($_POST["wc_".$pokoj->__get('id_pok')])){
+        $pokoj->__set('wc', 1);
+      } else {
+        $pokoj->__set('wc', 0);
+      }
+      if(isset($_POST["prszynic_".$pokoj->__get('id_pok')])){
+        $pokoj->__set('prszynic', 1);
+      } else {
+        $pokoj->__set('prszynic', 0);
+      }
+      if(isset($_POST["wanna_".$pokoj->__get('id_pok')])){
+        $pokoj->__set('wanna', 1);
+      } else {
+        $pokoj->__set('wanna', 0);
+      }
+      if(isset($_POST["jacuzzi_".$pokoj->__get('id_pok')])){
+        $pokoj->__set('jacuzzi', 1);
+      } else {
+        $pokoj->__set('jacuzzi', 0);
+      }
+      if(isset($_POST["klimatyzacja_".$pokoj->__get('id_pok')])){
+        $pokoj->__set('klimatyzacja', 1);
+      } else {
+        $pokoj->__set('klimatyzacja', 0);
+      }
+      if(isset($_POST["internet_".$pokoj->__get('id_pok')])){
+        $pokoj->__set('internet', 1);
+      } else {
+        $pokoj->__set('internet', 0);
+      } if(isset($_POST["usun_zdjecie_".$pokoj->__get('id_pok')])){
+        $pokoj->__set('zdjecie', NULL);
+      }
+      if(isset($_POST["zmien_zdjecie_".$pokoj->__get('id_pok')])){
+        $zdjecie = 'zdjecie_'.$pokoj->__get('id_pok');
+        $isFileUploaded = isset($_FILES[$zdjecie]['tmp_name']) && $_FILES[$zdjecie]['tmp_name'] != "";
+        $pokoj->__set('zdjecie', $isFileUploaded?file_get_contents($_FILES[$zdjecie]['tmp_name']):"");
+      }
+      return $pokoj;
+  }
+
+  private function pobierzDostepnePokoje($dostepneNoclegownie){
+    $dostepneNoclegownieIds = array();
+    foreach($dostepneNoclegownie as $noclegownia){
+      $dostepneNoclegownieIds[] = $noclegownia->__get('id');
+    }
+    return Pokoj::doSelectByIdNoclegowni($dostepneNoclegownieIds);
+  }
+
+  private function podzielRezerwacjeNaNoclegownie($wszystkieAktywneRezerwacje){
+    $aktywneRezerwacje = array();
+    foreach($wszystkieAktywneRezerwacje as $rezerwacja){
+      $aktywneRezerwacje[$rezerwacja['noclegownia']->__get('id')][] = $rezerwacja;
+    }
+    return $aktywneRezerwacje;
+  }
+}
+?>
